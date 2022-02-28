8e9ab98474aff990f859271b2a156f560eed8301
Rıdvan KÜNTUĞ <ridvankuntug@gmail.com>
2022-02-28 10:15:15 +0300

    fix

--- SiteManagementApplication/Operations/UserOperations/Commands/AddUser/AddUserValidator.cs
+++ SiteManagementApplication/Operations/UserOperations/Commands/AddUser/AddUserValidator.cs
@@ -12,7 +12,7 @@ namespace SiteManagementApplication.Operations.UserOperations.Commands.AddUser
             RuleFor(c => c.Model.UserTc.ToString()).NotNull().Length(11).WithMessage("Tc 11 hane olmalı.");
             RuleFor(c => c.Model.Email).NotNull().EmailAddress().MaximumLength(30).WithMessage("Mail 30 dan uzun olamaz.");
             RuleFor(c => c.Model.PhoneNumber).NotNull().MaximumLength(10).WithMessage("Telefon numarası 10 hane olmalı.");
-            RuleFor(c => c.Model.PasswordHash).NotNull().MinimumLength(6).WithMessage("Şifre 6 handen kısa olmalı.");
+            RuleFor(c => c.Model.PasswordHash).NotNull().MinimumLength(6).WithMessage("Şifre 6 handen kısa olmamalı.");
             RuleFor(c => c.Model.UserVehicle).MaximumLength(20).WithMessage("Araç özelliği 20 handen kısa olmalı.");
             RuleFor(c => c.Model.IsAdmin).NotNull();
         }
